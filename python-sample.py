d5811a3ccddc990aacb8230c2fc04246737d4433
geeknow112 <geeknow112@gmail.com>
2022-04-24 21:35:37 +0900

    file read

--- python-sample.py
+++ python-sample.py
@@ -4,7 +4,7 @@ a = np.array([1, 2])
 b = np.array([3, 4])
 print(a + b)
 
-f = open('./tweets/baby-step.tweet', 'r', encoding='SJIS')
+f = open('./tweets/baby-step.tweet', 'r', encoding='UTF-8')
 data = f.read()
 print(data)
 f.close()
