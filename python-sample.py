ff0058fca904357f8e772ac9171573051254682c
geeknow112 <geeknow112@gmail.com>
2022-04-24 21:51:09 +0900

    upd tweepyw

--- python-sample.py
+++ python-sample.py
@@ -9,3 +9,34 @@ data = f.read()
 print(data)
 f.close()
 
+
+# ツイートを取得
+import tweepy
+import time
+import glob
+
+# 取得した各種キーを格納---------------------------------------------
+consumer_key ="6X5WoqpjShCgPVvipQQ6aEVLm"
+consumer_secret ="FdRkKXpdcM3WUD3cNHet1FeBSEJbqt40bQTe2uYvUqQAqDtsau"
+access_token="3009736928-IvceHOtALGPI3b0Od2kWE7r1OKxP8fbVpIwsfIP"
+access_token_secret ="WYhXGZ07p0ZNqH6LMNfSD19OQob08DSul1lsSrIwvly2U"
+
+# Twitterオブジェクトの生成
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+#-------------------------------------------------------------------
+
+#ツイートを投稿
+#api.update_status("シグナル通り翌日急落。。。")
+
+#画像付きツイート
+#api.update_with_media(status = '本日のチャート\n9101', filename = '/var/www/tmp/charts/20211210/9101.png')
+
+#from gs import gsp
+#data = gsp.getTweetList()
+#print(data)
+if data is not None:
+    api.update_status(data)
+
+
